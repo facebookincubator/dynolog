3ed35e49d3953e4977c675d22c787c8d06238363
jonesz <jonesz@fb.com>
2022-10-04 21:49:55 -0700

    Use recommended copyright header in source files //dynolog
    Summary:
    Remove exiting header if it exsits:
    ```
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' |  xargs -n1 sed -i 's/\/\/.*Copyright.*//'
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i 's/\/\/.*(c)*//'
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i '2i\/\/\n\/\/ This source code is licensed under the MIT license found in the\n\/\/ LICENSE file in the root directory of this source tree.'
    ```
    
    Add recommended header:
    ```
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i '1i\/\/ Copyright (c) Meta Platforms, Inc. and affiliates.'
    ```
    
    Differential Revision: https://internalfb.com/D40036346
    
    fbshipit-source-id: a2c9c082be9ae25cff753bebc719e681b587fec4

--- tests/KernelCollecterTest.cpp
+++ tests/KernelCollecterTest.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include <sys/stat.h>
 
 #include <glog/logging.h>
