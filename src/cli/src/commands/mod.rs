c555b3b199d42da97f781565f97f7c476dd53545
Zachary Jones <jonesz@meta.com>
2022-10-04 21:52:43 -0700

    Use recommended copyright header in source files //dynolog (#22)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/22
    
    Remove exiting header if it exsits:
    ```
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i 's/\/\/.*Copyright.*//'
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i 's/\/\/.*(c).*//'
    ```
    
    Add recommended header:
    ```
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i '1i\/\/ Copyright (c) Meta Platforms, Inc. and affiliates.'
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i '2i\/\/\n\/\/ This source code is licensed under the MIT license found in the\n\/\/ LICENSE file in the root directory of this source tree.\n'
    ```
    
    Reviewed By: briancoutinho
    
    Differential Revision: D40036346
    
    fbshipit-source-id: a869b341b697acd37ab289d1dee157da960d6acc

--- src/cli/src/commands/mod.rs
+++ src/cli/src/commands/mod.rs
@@ -1,4 +1,7 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
 
 // Export all command submodules to be used in main.rs
 // Note: This "intermediate" commands module is purely for organizational purposes.
