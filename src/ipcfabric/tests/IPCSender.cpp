b9c82b337e6df9d2e7508c36e1530722b9a4f1d5
Zachary Jones <jonesz@meta.com>
2022-10-04 22:49:40 -0700

    Use recommended copyright header in source files //dynolog (#22)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/22
    
    Remove exiting header if it exsits:
    ```
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i 's/\/\/.*Copyright.*//'
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i 's/\/\/.*(c).*//'
    ```
    
    Add recommended header:
    ```
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i '1i\/\/ Copyright (c) Meta Platforms, Inc. and affiliates.'
    find dynolog/ -name '*.rs' -o -name '*.cpp' -o -name '*.h' | xargs -n1 sed -i '2i\/\/\n\/\/ This source code is licensed under the MIT license found in the\n\/\/ LICENSE file in the root directory of this source tree.\n'
    ```
    
    Reviewed By: briancoutinho
    
    Differential Revision: D40036346
    
    fbshipit-source-id: 4faaa409cb4fba68baac84d77facfbd002805e8b

--- src/ipcfabric/tests/IPCSender.cpp
+++ src/ipcfabric/tests/IPCSender.cpp
@@ -1,4 +1,7 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
 
 // @lint-ignore-every CLANGTIDY facebook-hte-RelativeInclude
 #include "FabricManager.h" // @manual=//dynolog/src/ipcfabric:ipcfabric
