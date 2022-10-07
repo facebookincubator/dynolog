0323566efb6a6e9ce6b95ff4d617421d57e5f4de
Zachary Jones <jonesz@meta.com>
2022-10-06 22:34:38 -0700

    Use recommended copyright header in source files //hbt/src/{intel_pt,ringbuffer,tagstack,utils}
    Summary:
    Remove exiting header if it exists:
    ```find hbt/src/{intel_pt,ringbuffer,tagstack,utils} -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs -n1 sed -i 's/\/\/.*Copyright.*//'
    find hbt/src/{intel_pt,ringbuffer,tagstack,utils} -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs -n1 sed -i 's/\/\/.*(c).*//'
    ```
    
    Add recommended header:
    ```
    find hbt/src/{intel_pt,ringbuffer,tagstack,utils} -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs -n1 sed -i '1i\/\/ Copyright (c) Meta Platforms, Inc. and affiliates.'
    find hbt/src/{intel_pt,ringbuffer,tagstack,utils} -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs -n1 sed -i '2i\/\/\n\/\/ This source code is licensed under the MIT license found in the\n\/\/ LICENSE file in the root directory of this source tree.\n'
    ```
    
    Python files edited by hand.
    
    Differential Revision: D40169049
    
    fbshipit-source-id: 5c1f8984b7bc3521a218035544a4879b5b8d7f97

--- hbt/src/intel_pt/IptEventBuilder.cpp
+++ hbt/src/intel_pt/IptEventBuilder.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include "hbt/src/intel_pt/IptEventBuilder.h"
 #include "hbt/src/perf_event/PmuEvent.h"
 
