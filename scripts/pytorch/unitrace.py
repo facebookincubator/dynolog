dcde40664e86a9de4e79498c0bf2458f6b0bf6ae
bcoutinho <bcoutinho@meta.com>
2022-10-27 18:58:22 -0700

    Pytorch scripts2 (#46)
    Summary:
    1. Adds scripts for pytorch tracing
    2. Update documentation for pytorch
    
    # Tests
    
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/46
    
    Test Plan:
    ## Test slurm job wrapper
    ```
    $> ./scripts/slurm/run_with_dyno_wrapper.sh ls
    
    Starting dynolog
    + dyno_pid=108862
    + echo ' dyno pid = 108862'
     dyno pid = 108862
    + sleep 2s
    + ./build/dynolog/src//dynolog --enable_ipc_monitor
    ...
    + echo 'Running command'
    Running command
    + export KINETO_CONFIG=/private/home/bcoutinho//libkineto.conf
    + KINETO_CONFIG=/private/home/bcoutinho//libkineto.conf
    + export KINETO_USE_DAEMON=1
    + KINETO_USE_DAEMON=1
    + ls
    CHANGELOG.md  CMakeLists.txt  CODE_OF_CONDUCT.md  CONTRIBUTING.md  LICENSE  README.md  build  cli  dynolog  hbt  scripts  testing  third_party
    + echo 'Cleaning up dynolog'
    Cleaning up dynolog
    + kill -9 108862
    ```
    ## Test unitrace.py
    ```
    $> ./scripts/pytorch/unitrace.py 65738676 ./test
    
    Tracing job: 65738676 to output dir: /private/home/bcoutinho/dynolog_oss/dynolog/test
              65738676        w3    train patelyas  R 1-09:38:00     16 learnfair[1621,1623,1626,1628-1629,1631,1643-1645,1874-1876,1885-1888]
    Found hosts string = learnfair[1621,1623,1626,1628-1629,1631,1643-1645,1874-1876,1885-1888]
    Hosts = ['learnfair1621', 'learnfair1623', 'learnfair1626', 'learnfair1628', 'learnfair1629', 'learnfair1631', 'learnfair1643', 'learnfair1644', 'learnfair1645', 'learnfair1874', 'learnfair1875', 'learnfair1876', '
    learnfair1885', 'learnfair1886', 'learnfair1887', 'learnfair1888']
    Running cmd: ./build/release/dyno --hostname learnfair1621 gputrace --job-id 65738676 --log-file /private/home/bcoutinho/dynolog_oss/dynolog/test/libkineto_trace_learnfair1621.json
    ...
    ```
    
    Differential Revision: D40746681
    
    Pulled By: briancoutinho
    
    fbshipit-source-id: dda43d77d0a7af8bc8c8f5ae0dff2119310950ff

---
+++ scripts/pytorch/unitrace.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import shutil
+import subprocess
+import sys
+
+DYNO_BUILD_PATH = "./build/release/dyno"
+SQUEUE_BIN = "/usr/local/bin/squeue"
+SCONTROL_BIN = "/usr/local/bin/scontrol"
+
+
+def dyno_bin():
+    # first seach in PATH
+    binpath = shutil.which("dyno")
+    if binpath is not None:
+        return binpath
+
+    # try looking up build based binary
+    if os.path.isfile(DYNO_BUILD_PATH):
+        return DYNO_BUILD_PATH
+
+    return None
+
+
+def get_hosts(job):
+    squeue_out = subprocess.check_output([SQUEUE_BIN, "-j", job], text=True)
+
+    if squeue_out == "":
+        print("Failed to run squeue command")
+        return
+
+    last_line = squeue_out.splitlines()[-1]
+    print(last_line)
+    # 64533952  learnlab     bash ansongni  R    2:44:49      1 learnfair2270
+    host_str = last_line.split()[7]
+    print(f"Found hosts string = {host_str}")
+
+    # we may need to decode the host string
+    # learnfair[1758,1765,1767-1768,1809-1812]
+    hosts = []
+
+    if "[" not in host_str:
+        hosts = [host_str]
+    else:
+        scontrol_out = subprocess.check_output(
+            [SCONTROL_BIN, "show", "hostnames", host_str], text=True
+        )
+        hosts = scontrol_out.splitlines()
+
+    print(f"Hosts = {hosts}")
+    return hosts
+
+
+def main():
+
+    if len(sys.argv) <= 2:
+        print(f"Usage: {sys.argv[0]} [slurm_job_id] [output_dir]")
+        sys.exit(0)
+
+    dyno_cmdline = dyno_bin()
+    if dyno_cmdline is None:
+        print("Error: could not find dyno cmd line in path or at", DYNO_BUILD_PATH)
+        print("Please run this script from dynolog project root or install dyno.")
+        sys.exit(0)
+
+    job = sys.argv[1]
+    outdir = sys.argv[2]
+
+    if "_" in job:
+        print(
+            "Currently dynolog does not support job names with '_' for "
+            "trace targeting, only numeric job ids are supported"
+        )
+        sys.exit(1)
+
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
+    outdir = os.path.abspath(outdir)
+
+    print(f"Tracing job: {job} to output dir: {outdir}")
+
+    for host in get_hosts(job):
+        cmd = (
+            f"{dyno_cmdline} --hostname {host} gputrace "
+            f"--job-id {job} --log-file {outdir}/libkineto_trace_{host}.json"
+        )
+        print(f"Running cmd: {cmd}")
+        os.system(cmd)
+
+
+if __name__ == "__main__":
+    main()
