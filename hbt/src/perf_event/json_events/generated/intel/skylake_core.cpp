1d86028c5b85d14a0eba853ad2a3255d5a5d53ed
Alston Tang <alston64@meta.com>
2023-06-27 11:31:11 -0700

    fix flops metrics defintion (#149)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/149
    
    - fix the incorrect single/double flops instructions definitions in BuiltinMetrics
    - regenerate events import function because some new events are now used in BuiltinMetrics
    
    Reviewed By: briancoutinho
    
    Differential Revision: D46827650
    
    fbshipit-source-id: e224bec9467d7de60ac5a869fb2b3399b5908673

--- hbt/src/perf_event/json_events/generated/intel/skylake_core.cpp
+++ hbt/src/perf_event/json_events/generated/intel/skylake_core.cpp
@@ -3616,7 +3616,7 @@ Note: Invoking MITE requires two or three cycles delay.)",
       std::nullopt // Errata
       ));
 
-#ifdef HBT_ADD_ALL_GENERATED_EVENTS
+  // Event FP_ARITH_INST_RETIRED.128B_PACKED_SINGLE is allowlisted
   pmu_manager.addEvent(std::make_shared<EventDef>(
       PmuType::cpu,
       "FP_ARITH_INST_RETIRED.128B_PACKED_SINGLE",
@@ -3629,7 +3629,6 @@ Note: Invoking MITE requires two or three cycles delay.)",
       EventDef::IntelFeatures{},
       std::nullopt // Errata
       ));
-#endif // HBT_ADD_ALL_GENERATED_EVENTS
 
   // Event FP_ARITH_INST_RETIRED.256B_PACKED_DOUBLE is allowlisted
   pmu_manager.addEvent(std::make_shared<EventDef>(
@@ -3660,7 +3659,7 @@ Note: Invoking MITE requires two or three cycles delay.)",
       ));
 #endif // HBT_ADD_ALL_GENERATED_EVENTS
 
-#ifdef HBT_ADD_ALL_GENERATED_EVENTS
+  // Event FP_ARITH_INST_RETIRED.256B_PACKED_SINGLE is allowlisted
   pmu_manager.addEvent(std::make_shared<EventDef>(
       PmuType::cpu,
       "FP_ARITH_INST_RETIRED.256B_PACKED_SINGLE",
@@ -3673,7 +3672,6 @@ Note: Invoking MITE requires two or three cycles delay.)",
       EventDef::IntelFeatures{},
       std::nullopt // Errata
       ));
-#endif // HBT_ADD_ALL_GENERATED_EVENTS
 
 #ifdef HBT_ADD_ALL_GENERATED_EVENTS
   pmu_manager.addEvent(std::make_shared<EventDef>(
