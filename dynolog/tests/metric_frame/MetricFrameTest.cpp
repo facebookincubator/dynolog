b7996658e4e62e382c6eacf23963ef951d3bf240
Sam Crossley <scrossley@meta.com>
2025-02-15 16:43:13 -0800

    Add method to extract raw samples from MetricSlice (#345)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/345
    
    Raw samples are useful for certain bespoke data operations, such as instantiating datasketches, or plotting a trace.
    
    Reviewed By: bigzachattack
    
    Differential Revision: D69647262
    
    fbshipit-source-id: 35aef7e5465e1d45fe8a9255a53338d9b1d67a36

--- dynolog/tests/metric_frame/MetricFrameTest.cpp
+++ dynolog/tests/metric_frame/MetricFrameTest.cpp
@@ -126,6 +126,7 @@ TEST(MetricSeriesSliceTest, metricFrameMapSmokeTest) {
   EXPECT_NEAR(seriesSlice1->rate<double>(1s), 0.01667, 1e-3);
   EXPECT_EQ(seriesSlice1->rate<int>(1min), 1);
   EXPECT_EQ(seriesSlice1->diff(), 5);
+  EXPECT_EQ(seriesSlice1->raw(), std::vector<long>({44, 45, 46, 47, 48, 49}));
 
   EXPECT_NEAR(seriesSlice2->avg<double>(), 28.4, 1e-3);
   EXPECT_NEAR(seriesSlice2->percentile(0.4), 27.9, 1e-3);
@@ -139,6 +140,9 @@ TEST(MetricSeriesSliceTest, metricFrameMapSmokeTest) {
   EXPECT_NEAR(seriesSlice3->rate<double>(1s), 0.01667, 1e-3);
   EXPECT_EQ(seriesSlice3->rate<int>(1min), 1);
   EXPECT_EQ(seriesSlice3->diff(), 5);
+  EXPECT_EQ(
+      seriesSlice3->raw(),
+      std::vector<unsigned long>({44, 45, 46, 47, 48, 49}));
 
   EXPECT_TRUE(frameMap.eraseSeries(series1Key));
   EXPECT_FALSE(frameMap.eraseSeries("not_exist"));
@@ -207,6 +211,7 @@ TEST(MetricSeriesSliceTest, metricFrameVectorSmokeTest) {
   EXPECT_NEAR(seriesSlice1->rate<double>(1s), 0.01667, 1e-3);
   EXPECT_EQ(seriesSlice1->rate<int>(1min), 1);
   EXPECT_EQ(seriesSlice1->diff(), 5);
+  EXPECT_EQ(seriesSlice1->raw(), std::vector<long>({44, 45, 46, 47, 48, 49}));
 
   EXPECT_NEAR(seriesSlice2->avg<double>(), 28.4, 1e-3);
   EXPECT_NEAR(seriesSlice2->percentile(0.4), 27.9, 1e-3);
@@ -220,6 +225,9 @@ TEST(MetricSeriesSliceTest, metricFrameVectorSmokeTest) {
   EXPECT_NEAR(seriesSlice3->rate<double>(1s), 0.01667, 1e-3);
   EXPECT_EQ(seriesSlice3->rate<int>(1min), 1);
   EXPECT_EQ(seriesSlice3->diff(), 5);
+  EXPECT_EQ(
+      seriesSlice3->raw(),
+      std::vector<unsigned long>({44, 45, 46, 47, 48, 49}));
 }
 
 TEST(MetricSeriesSliceTest, perfReadValuesCompabilityTest) {
