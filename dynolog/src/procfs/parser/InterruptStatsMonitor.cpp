d7e509fbb29d625d239adb2383ce3a31a06aa23a
Abhijeet Shedge <abhishedge@meta.com>
2025-05-14 16:49:47 -0700

    Add a new monitor for procfs interrupts (#373)
    Summary:
    
    We are following the newly established monitor-publisher pattern of reading and exposing the perf metrics.
    The logic to process interrupt data will be movced to a separate monitor class of type MonitorBase.
    
    What to expect in upcoming diffs?
    - Unit test for the new monitor calss
    - Integration with the Server.cpp
    - Cleanup of the old code
    
    Differential Revision: D74662495

---
+++ dynolog/src/procfs/parser/InterruptStatsMonitor.cpp
@@ -0,0 +1,151 @@
+#include "dynolog/src/procfs/parser/InterruptStatsMonitor.h"
+#include "folly/Range.h"
+#include "folly/String.h"
+#include "folly/logging/xlog.h"
+#include "folly/small_vector.h"
+
+namespace facebook {
+namespace dynolog {
+
+using Granularity = InterruptStatsMonitor::Granularity;
+
+InterruptStatsMonitor::InterruptStatsMonitor(
+    std::shared_ptr<TTicker> ticker,
+    const std::string& rootDir)
+    : MonitorBase<TTicker>(std::move(ticker), "InterruptStatsMonitor", {}),
+      rootDir_(rootDir) {
+  cpuCount_ = (int)sysconf(_SC_NPROCESSORS_CONF);
+}
+
+InterruptStatsMonitor InterruptStatsMonitor::makeTestClass(
+    const std::string& rootDir) {
+  std::shared_ptr<InterruptStatsMonitor::TTicker> intTicker =
+      std::make_shared<InterruptStatsMonitor::TTicker>();
+  return InterruptStatsMonitor(intTicker, rootDir);
+}
+
+InterruptStats InterruptStatsMonitor::getInterruptStatsPerMinute() {
+  return interruptStats_[static_cast<int>(Granularity::MINUTE)];
+}
+
+InterruptStats InterruptStatsMonitor::getInterruptStatsPerSecond() {
+  return interruptStats_[static_cast<int>(Granularity::SECOND)];
+}
+
+InterruptStats InterruptStatsMonitor::getInterruptStats(
+    Granularity granularity) {
+  if (granularity == Granularity::MINUTE) {
+    return getInterruptStatsPerMinute();
+  } else if (granularity == Granularity::SECOND) {
+    return getInterruptStatsPerSecond();
+  } else {
+    throw std::runtime_error(
+        "InterruptStatsMonitor doesn't support requested granularity");
+  }
+}
+
+void InterruptStatsMonitor::tick(TMask mask) {
+  std::unique_lock lock(dataLock_);
+  if (TTicker::is_major_tick(mask)) {
+    InterruptStats freshStats = interruptsRefresh();
+    interruptStats_.insert_or_assign(
+        static_cast<int>(Granularity::MINUTE), freshStats);
+  } else if (TTicker::is_minor_tick(mask)) {
+    InterruptStats freshStats = interruptsRefresh();
+    interruptStats_.insert_or_assign(
+        static_cast<int>(Granularity::SECOND), freshStats);
+  }
+}
+
+InterruptStats InterruptStatsMonitor::interruptsRefresh() {
+  std::string fullPath = rootDir_ + "/proc/interrupts";
+  folly::small_vector<folly::StringPiece> v;
+
+  int64_t eth0IntrpsSum = 0, eth0Intrps;
+  stats.eth0Intrps = 0;
+
+  try {
+    if (!std::filesystem::exists(fullPath)) {
+      XLOG(ERR) << "Path " << fullPath << " does not exist";
+      return stats;
+    }
+    std::ifstream file(fullPath);
+    if (!file.is_open()) {
+      XLOG(ERR) << "Failed to open the file  " << fullPath;
+      return stats;
+    }
+
+    // enough for  /proc/interrupts line
+    const int bufferSize = 1024;
+    char buf[bufferSize];
+    while (file.getline(buf, bufferSize)) {
+      // expected buf format where xxxx refers to a TLB shootdown value for a
+      // core One value for one core. So, the number of values should match
+      // cpuCount_
+      //   TLB xxxx xxxx xxxx .... xxxx xxxx TLB shootdowns
+      if (strstr(buf, "TLB shootdowns")) {
+        folly::split(' ', buf, v, true);
+        int64_t tlbshootdowns = 0;
+        size_t valueCount = 0;
+        for (const auto& tlbshootdownStr : v) {
+          folly::tryTo<int64_t>(tlbshootdownStr)
+              .then([&tlbshootdowns, &valueCount](int64_t tlbshootdown) {
+                tlbshootdowns += tlbshootdown;
+                valueCount++;
+              });
+        }
+        if (valueCount == cpuCount_) {
+          stats.tlbshootdowns = tlbshootdowns - stats.tlbshootdownsPrev;
+          stats.tlbshootdowns += (stats.tlbshootdowns < 0) *
+              (((stats.tlbshootdowns * -1) - 1) / UINT32_MAX + 1) * UINT32_MAX;
+          stats.tlbshootdownsPrev = tlbshootdowns;
+        } else {
+          XLOG(WARN) << "CPU count from procfs interrupts: " << valueCount
+                     << " Expected: " << cpuCount_;
+        }
+        v.clear();
+      }
+
+      // expected buf format where xxxx refers to a eth0Intrp value for a core
+      // One value for one core. So, the number of values should match cpuCount_
+      //   IRQ#: xxxx xxxx xxxx .... xxxx xxxx PCI-MSI-edge eth0-#
+      // In the some versions of kernel, PCI-MSI-edge part may be separated by a
+      // space
+      if (strstr(buf, "eth0-") || strstr(buf, "mlx5_comp")) {
+        folly::split(' ', buf, v, true);
+        int64_t eth0IntrpRow = 0;
+        size_t valueCount = 0;
+        for (const auto& eth0IntrpStr : v) {
+          folly::tryTo<int64_t>(eth0IntrpStr)
+              .then([&eth0IntrpRow, &valueCount](int64_t eth0Intrp) {
+                eth0IntrpRow += eth0Intrp;
+                valueCount++;
+              });
+        }
+        if (valueCount == cpuCount_) {
+          eth0IntrpsSum += eth0IntrpRow;
+        } else {
+          XLOG(WARN) << "CPU count from procfs interrupts: " << valueCount
+                     << " Expected: " << cpuCount_;
+        }
+        v.clear();
+      }
+    }
+    if (eth0IntrpsSum) {
+      eth0Intrps = eth0IntrpsSum - stats.eth0IntrpsPrev;
+      eth0Intrps += (int64_t)(eth0Intrps < 0) *
+          (((eth0Intrps * -1) - 1) / UINT32_MAX + 1) * UINT32_MAX;
+      stats.eth0IntrpsPrev = eth0IntrpsSum;
+      stats.eth0Intrps = eth0Intrps;
+    }
+
+    file.close();
+  } catch (const std::exception& e) {
+    XLOG(ERR) << "Error in reading the procfs interrupts file: " << fullPath
+              << " Error: " << e.what();
+  }
+  return stats;
+}
+
+} // namespace dynolog
+} // namespace facebook
