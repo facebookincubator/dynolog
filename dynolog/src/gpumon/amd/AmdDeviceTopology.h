a0cc9c28f0fed34473594581b194eb6f6851639d
Alston Tang <alston64@meta.com>
2025-07-02 11:14:03 -0700

    add logic for constructing device mappings for AMD GPUs (#402)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/402
    
    AMD exposes some info by its own driver code through /sys/class/kfd directory. we will need relying on info from here to determine logical device and physical device correlation.
    
    Reviewed By: haowangludx
    
    Differential Revision: D71225456
    
    fbshipit-source-id: 1ba1e008f014f2e619209d22b33f425795c812e1

---
+++ dynolog/src/gpumon/amd/AmdDeviceTopology.h
@@ -0,0 +1,56 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <cstdint>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace dynolog {
+namespace gpumon {
+namespace amdgpu {
+
+class LogicalDevice {
+ public:
+  uint32_t getKfdNodeId() const {
+    return kfdNodeId_;
+  }
+  uint64_t getUniqueId() const {
+    return uniqueId_;
+  }
+  int getMinorId() const {
+    return minorId_;
+  }
+
+  /**
+   * parse AMD GPU topology information from sysfs.
+   *
+   * reads device information from KFD topology nodes directory.
+   * each node directory contains a properties file with device details.
+   * only nodes with SIMD units (actual GPUs) are included.
+   *
+   * @param root base sysfs path, defaults to "/sys/class/kfd"
+   * @return vector of LogicalDevice instances for each logical GPU
+   */
+  static std::vector<LogicalDevice> parseTopologyNodes(
+      const std::filesystem::path& root = "/sys/class/kfd");
+
+ protected:
+  LogicalDevice(uint32_t kfdNodeId, uint64_t uniqueId, int minorId)
+      : kfdNodeId_(kfdNodeId), uniqueId_(uniqueId), minorId_(minorId) {}
+  uint32_t kfdNodeId_;
+  uint64_t uniqueId_;
+  int minorId_;
+};
+
+class PhysicalDevice {
+ protected:
+  uint64_t uniqueId_;
+  std::string pciAddr_;
+  int oamId_;
+};
+
+} // namespace amdgpu
+} // namespace gpumon
+} // namespace dynolog
---
+++ dynolog/tests/gpumon/amd/AmdDeviceTopologyTest.cpp
@@ -0,0 +1,42 @@
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "dynolog/src/gpumon/amd/AmdDeviceTopology.h"
+
+using namespace dynolog::gpumon;
+
+constexpr char kTestRoot[] = "TESTROOT";
+
+std::string get_test_root() {
+  if (const char* env_p = std::getenv(kTestRoot)) {
+    return std::filesystem::path(env_p) / "sys" / "class" / "kfd";
+  }
+  LOG(ERROR) << "Missing test root env: " << kTestRoot;
+  return "";
+}
+
+class AmdDeviceTopologyTest : public ::testing::Test {
+ protected:
+  const std::filesystem::path testRoot = get_test_root();
+};
+
+TEST_F(AmdDeviceTopologyTest, ParseKfdNodes) {
+  auto devices =
+      dynolog::gpumon::amdgpu::LogicalDevice::parseTopologyNodes(testRoot);
+  EXPECT_EQ(devices.size(), 3);
+
+  // Check first GPU node
+  EXPECT_EQ(devices[0].getKfdNodeId(), 1);
+  EXPECT_EQ(devices[0].getMinorId(), 177);
+  EXPECT_EQ(devices[0].getUniqueId(), 9847564688237960509ULL);
+
+  // Check second GPU node
+  EXPECT_EQ(devices[1].getKfdNodeId(), 2);
+  EXPECT_EQ(devices[1].getMinorId(), 178);
+  EXPECT_EQ(devices[1].getUniqueId(), 9847564688237960510ULL);
+
+  // Check second GPU node
+  EXPECT_EQ(devices[2].getKfdNodeId(), 3);
+  EXPECT_EQ(devices[2].getMinorId(), 179);
+  EXPECT_EQ(devices[2].getUniqueId(), 9847564688237960510ULL);
+}
