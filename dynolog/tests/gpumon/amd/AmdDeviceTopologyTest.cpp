36a120b095194c319c7b0fec434f66f893f8160e
Yao Zhou <yaoz@meta.com>
2025-09-08 16:33:13 -0700

    Apply LICENSELINT to OSS Dynolog (#443)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/443
    
    We received an alert that we are failing to meeting OSS requirements due to missing or incorrect headers. I found out that we actually have LICENSELINT (https://www.internalfb.com/wiki/Linting/License_Lint/) that can automatically fix the headers and should help catching this issue at diff time in the future.
    
    Reviewed By: cmgrace
    
    Differential Revision: D81940929
    
    fbshipit-source-id: 98ae65d7835e6f2d51281c168a49e500faa04530

--- dynolog/tests/gpumon/amd/AmdDeviceTopologyTest.cpp
+++ dynolog/tests/gpumon/amd/AmdDeviceTopologyTest.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 
