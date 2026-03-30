6cb26d895a0e419ffe8d81820b702532faae3f14
Zachary Jones <jonesz@meta.com>
2026-03-30 16:05:30 -0700

    Add #pragma once to MPMCBenchmark.hpp (#521)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/dynolog/pull/521
    
    Add `#pragma once` include guard to `MPMCBenchmark.hpp` to fix the `facebook-hte-MultipleIncludeGuardMissing` lint issue.
    
    Reviewed By: yaoz08
    
    Differential Revision: D98681573
    
    fbshipit-source-id: 7309e90adef71836d5ff8337816b7dfb031a8ea7

--- hbt/src/ringbuffer/benchmarks/MPMCBenchmark.hpp
+++ hbt/src/ringbuffer/benchmarks/MPMCBenchmark.hpp
@@ -3,6 +3,8 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
+#pragma once
+
 #include "hbt/src/common/System.h"
 #include "hbt/src/ringbuffer/benchmarks/Consumer.hpp"
 #include "hbt/src/ringbuffer/benchmarks/Data.hpp"
